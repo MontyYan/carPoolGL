cc9f590631f2ffc15ae264909532f372a26e4135
MontyYan <839343988@qq.com>
2020-03-16 10:48:11 +0800

    Main Page

--- app/src/main/java/com/example/carpoolgl/MainActivity.java
+++ app/src/main/java/com/example/carpoolgl/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onDestroy();
         nowLocP.destory();
         nowLocP.detachView();
-
     }
 
     @Override
