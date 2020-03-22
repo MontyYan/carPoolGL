c0b828683b41bebb1a71e221ac43e7fc81d5f7e6
MontyYan <839343988@qq.com>
2020-03-22 12:34:32 +0800

    choose location V

--- app/src/main/java/com/example/carpoolgl/dragLoc/chooseLocView.java
+++ app/src/main/java/com/example/carpoolgl/dragLoc/chooseLocView.java
@@ -1,7 +1,10 @@
 package com.example.carpoolgl.dragLoc;
 
 
+import com.amap.api.services.core.LatLonPoint;
+
 public interface chooseLocView {
     void LocInfo_SetText(String s);
 
+    void LocInfo_LatLon(LatLonPoint mPoint);
 }
