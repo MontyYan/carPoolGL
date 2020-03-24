26ad7b1fd39bd0073e6cbd8601f60f612daca89c
MontyYan <839343988@qq.com>
2020-03-24 15:50:01 +0800

    nowloc P

--- app/src/main/java/com/example/carpoolgl/nowLoc/nowLocPresenter.java
+++ app/src/main/java/com/example/carpoolgl/nowLoc/nowLocPresenter.java
@@ -8,6 +8,7 @@ import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
+import com.amap.api.services.core.LatLonPoint;
 
 public class nowLocPresenter {
     private AMapLocationClient mClient;
@@ -30,13 +31,30 @@ public class nowLocPresenter {
                 if(aMapLocation.getErrorCode()==0){
 //                    LatLng la = new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude());
                     StringBuffer buffer = new StringBuffer();
-                    buffer.append(aMapLocation.getCity() + " "
-                            + aMapLocation.getDistrict() + " "
-                            + aMapLocation.getStreet() + " "
-                            + aMapLocation.getStreetNum());
+//                    buffer.append(aMapLocation.getCity() + " "  //苏州市
+//                            + aMapLocation.getDistrict() + " "  //姑苏区
+//                            + aMapLocation.getStreet() + " "    //肇源弄
+//                            + aMapLocation.getStreetNum());     //33号
+                    buffer.append(aMapLocation.getDistrict() + " "  //姑苏区
+                                + aMapLocation.getAoiName());    //杨枝二村苏大家属区
                     Log.e("地理位置",buffer.toString());
+                    Log.e("地理位置",aMapLocation.getAddress());//江苏省苏州市姑苏区肇源弄33号靠近杨枝二村苏大家属区
+                    Log.e("地理位置",aMapLocation.getAoiName());//杨枝二村苏大家属区
+                    Log.e("地理位置",aMapLocation.getCity());//苏州市
+                    Log.e("地理位置",aMapLocation.getCountry());//中国
+                    Log.e("地理位置",aMapLocation.getDescription());//在杨枝二村苏大家属区附近
+                    Log.e("地理位置",aMapLocation.getDistrict());//姑苏区
+                    Log.e("地理位置f",aMapLocation.getFloor());//
+                    Log.e("地理位置de",aMapLocation.getLocationDetail());//#id:KLFZvY1RBTE52OEE0REFDcXRDa1VMS1pUQQ==#csid:3c449df8c24143b980146ab6b5d78b63
+                    Log.e("地理位置", aMapLocation.getPoiName());//杨枝二村苏大家属区
+                    Log.e("地理位置",aMapLocation.getProvider());//lbs
+                    Log.e("地理位置",aMapLocation.getProvince());//江苏省
+                    Log.e("地理位置",aMapLocation.getStreetNum());//33号
+
 
                     nowLocView.setGetonText(buffer.toString());
+//                    LatLonPoint latLonPoint = new LatLonPoint(aMapLocation.getLongitude(),aMapLocation.getLatitude());
+                    nowLocView.setLatLon(aMapLocation.getLongitude(),aMapLocation.getLatitude());
 //                    Toast.makeText(mContext, buffer.toString(), Toast.LENGTH_LONG).show();
                     mClient.stopLocation();
                 }
