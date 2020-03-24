26152269cb9b525783febbd42f11e6743a374228
MontyYan <839343988@qq.com>
2020-03-24 15:50:08 +0800

    nowloc V

--- app/src/main/java/com/example/carpoolgl/nowLoc/nowLocView.java
+++ app/src/main/java/com/example/carpoolgl/nowLoc/nowLocView.java
@@ -1,5 +1,8 @@
 package com.example.carpoolgl.nowLoc;
 
+import com.amap.api.services.core.LatLonPoint;
+
 public interface nowLocView {
     void setGetonText(String addr);
+    void setLatLon(double lat, double lon);
 }
