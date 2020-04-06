bfc0dedff03bb7e4f0b4bb8785a969cf5c303445
MontyYan <839343988@qq.com>
2020-04-06 20:33:07 +0800

    login activity

--- app/src/main/java/com/example/carpoolgl/LoginActivity.java
+++ app/src/main/java/com/example/carpoolgl/LoginActivity.java
@@ -1,17 +1,13 @@
 package com.example.carpoolgl;
 
-import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.dd.CircularProgressButton;
 import com.example.carpoolgl.base.baseActivity;
-import com.example.carpoolgl.bean.User;
+import com.example.carpoolgl.bean.User_;
 import com.example.carpoolgl.login.loginPresenter;
 import com.example.carpoolgl.login.loginView;
 import com.google.gson.Gson;
@@ -21,10 +17,6 @@ import com.rengwuxian.materialedittext.MaterialEditText;
 import java.util.List;
 
 import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class LoginActivity extends baseActivity<loginView,loginPresenter>
         implements View.OnClickListener,loginView {
@@ -86,9 +78,9 @@ public class LoginActivity extends baseActivity<loginView,loginPresenter>
 //        res_tv.setText(data);
         Log.i("LoginActivity","show:"+data);
         Gson gson = new Gson();
-        List<User> list = gson.fromJson(data, new TypeToken<List<User>>(){}.getType());
+        List<User_> list = gson.fromJson(data, new TypeToken<List<User_>>(){}.getType());
         Log.i("LoginActivity",list+"");
-        for(User u:list){
+        for(User_ u:list){
             Log.i("LoginActivity",u.getId()+"");
             Log.i("LoginActivity",u.getPhoneNum());
             Log.i("LoginActivity",u.getPassword());
