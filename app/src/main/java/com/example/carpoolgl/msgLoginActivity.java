efe6648fb51c2f64f5f9c4e1d689662f3399bf04
MontyYan <839343988@qq.com>
2020-04-07 22:48:36 +0800

    msg activity

---
+++ app/src/main/java/com/example/carpoolgl/msgLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.carpoolgl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class msgLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msg_login);
+    }
+}
