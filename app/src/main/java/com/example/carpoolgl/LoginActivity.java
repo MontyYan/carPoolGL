cb23f6ae1d08a6820d9313f10b80e420fec9e271
MontyYan <839343988@qq.com>
2020-04-14 22:45:25 +0800

    login activity

--- app/src/main/java/com/example/carpoolgl/LoginActivity.java
+++ app/src/main/java/com/example/carpoolgl/LoginActivity.java
@@ -1,19 +1,28 @@
 package com.example.carpoolgl;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
+import android.util.LogPrinter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.carpoolgl.base.baseActivity;
+import com.example.carpoolgl.bean.User;
 import com.example.carpoolgl.bean.User_;
+import com.example.carpoolgl.dataBase.MydbHelper;
 import com.example.carpoolgl.login.loginPresenter;
 import com.example.carpoolgl.login.loginView;
+import com.example.carpoolgl.login.mHandler;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.rengwuxian.materialedittext.MaterialEditText;
+
+import com.alibaba.fastjson.JSONObject;
 
 import java.util.List;
 
@@ -21,6 +30,9 @@ import okhttp3.MediaType;
 
 public class LoginActivity extends baseActivity<loginView,loginPresenter>
         implements View.OnClickListener,loginView {
+
+    //作为Message.what标识
+    private static final int UPDATA_DB = 1;
 
     private Button login_bt;
 
@@ -100,7 +112,8 @@ public class LoginActivity extends baseActivity<loginView,loginPresenter>
 //        loginP = new loginPresenter();
 //        loginP.attachView(this);
 //        getPresenter().login(phone_num_et.getText().toString(),password_et.getHelperText(),login_result);
-        getPresenter().login("",password_et.getHelperText(),login_result);
+        Log.i("user",phoneNum+" "+password_et.getHelperText());
+        getPresenter().login(phoneNum,password_et.getText().toString(),login_result);
     }
 
 
@@ -126,8 +139,72 @@ public class LoginActivity extends baseActivity<loginView,loginPresenter>
 //        }
     }
 
+
+    /*
+    * 得到登录请求结果
+    * {loginResult:0/1 ,
+    *  userInfo:...}
+    * */
     @Override
-    public void onloginResult(String result) {
+    public void onloginResult(final String result) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                JSONObject jsonObject = JSONObject.parseObject(result);
+                //获取loginRsult
+                Integer loginResult = jsonObject.getInteger("loginResult");
+                /*loginResult==1：登录成功
+                 * 解析userInfo，并将user信息存入本地数据库[user.db]
+                 * */
+                if(loginResult.equals(1)){
+                    //获取userInfo
+                    JSONObject jsonUser = jsonObject.getJSONObject("userInfo");
+                    Gson gson = new Gson();
+                    //使用Gson直接逆序列化
+                    User user = gson.fromJson(jsonUser.toString(), User.class);
+
+                    Looper.prepare();
+                    mHandler handler = new mHandler(getApplicationContext());
+                    Message msg = handler.obtainMessage();
+                    msg.what = UPDATA_DB;
+                    msg.obj = user;
+                    Log.i("LoginActivity","jsonObject>>>>>"+jsonObject.toJSONString());
+                    Log.i("LoginActivity","msg>>>>>"+msg.obj.toString());
+//                    Looper.prepare();
+                    handler.sendMessage(msg);
+                    Looper.loop();
+//            Looper.loop();
+//            handler.insert();
+//            //获取userInfo
+//            JSONObject jsonUser = jsonObject.getJSONObject("userInfo");
+//            Gson gson = new Gson();
+//            //使用Gson直接逆序列化
+//            User user = gson.fromJson(jsonUser.toString(), User.class);
+//            //获取数据库操作对象
+//            MydbHelper mydbHelper = new MydbHelper(this,"user.db",null,1);
+//            SQLiteDatabase db = mydbHelper.getWritableDatabase();
+//            //
+//            try{
+//                db.beginTransaction();
+//                String sql = "insert into user_info(phone,sequence,password,login_ways,register_date,sex,identity,emergency_phone,name,identity_id)"
+//                        +" values (?,?,?,?,?,?,?,?,?,?)";
+//                db.execSQL(sql,
+//                        new Object[]{user.getPhone(),user.getUserSeq(),user.getPassword(),user.getLoginWays(),user.getRegisterDate(),user.getSex(),user.getIdentity(),user.getEcyPhone(),user.getName(),user.getIdentityId()});
+//            }catch (Exception e){
+//                e.printStackTrace();
+//            }finally {
+//                db.setTransactionSuccessful();
+//                db.endTransaction();
+//                db.close();
+//            }
+                }
+
+            }
+        }).start();
 
     }
+
+
 }
