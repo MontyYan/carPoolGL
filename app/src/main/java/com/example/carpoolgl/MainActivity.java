3414acfae725d4d70524af06ebcea2ca59349526
MontyYan <839343988@qq.com>
2020-04-14 22:45:49 +0800

    Main activity

--- app/src/main/java/com/example/carpoolgl/MainActivity.java
+++ app/src/main/java/com/example/carpoolgl/MainActivity.java
@@ -6,19 +6,25 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.alibaba.fastjson.JSONObject;
 import com.amap.api.services.core.LatLonPoint;
+import com.example.carpoolgl.bean.User;
+import com.example.carpoolgl.dataBase.MydbHelper;
 import com.example.carpoolgl.nowLoc.nowLocPresenter;
 import com.example.carpoolgl.nowLoc.nowLocView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.gson.Gson;
 
 import org.xutils.view.annotation.ViewInject;
 
@@ -33,7 +39,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //drawelayout不覆盖
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigation;
+    private TextView QuitTv;
     private double mlatLong[]=new double[2];//记录当前位置的经纬度
+
+    //创建数据库测试
+    private Button cre_db_Test;
+    private MydbHelper mydbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +54,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         getOnTv.setOnClickListener(this);
         getOffTv = findViewById(R.id.getOff_TV);
         getOffTv.setOnClickListener(this);
+
+        cre_db_Test = findViewById(R.id.cre_db);
+        cre_db_Test.setOnClickListener(this);
+
+        mydbHelper = new MydbHelper(this,"user.db",null,1);
+//        QuitTv.findViewById(R.id.quit);
+//        QuitTv.setOnClickListener(this);
         InitDrawer();
         nowLocP = new nowLocPresenter(getApplicationContext());
         nowLocP.attachView(this);
@@ -56,7 +74,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.getOn_TV:
                 flag = false;
-//                Log.e("getOn_Tv","点击getOn_Tv");
                 intent = new Intent(MainActivity.this,SearchActivity.class);
                 intent.putExtra("Edit_select",flag);
                 intent.putExtra("nowLocation",nowLocation);
@@ -65,7 +82,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.getOff_TV:
                 flag = true;
-//                Log.e("getOff_Tv","点击getOff_Tv");
                 intent = new Intent(MainActivity.this,SearchActivity.class);
                 intent.putExtra("Edit_select",flag);
                 intent.putExtra("nowLocation",nowLocation);
@@ -73,28 +89,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
             case R.id.PersonalInfo:
-
                 mDrawerLayout.openDrawer(GravityCompat.START);
-//                drawer_fog.setVisibility(View.VISIBLE);
                 break;
-//            case R.id.drawer_fog:
-////                drawer_fog.setVisibility(View.INVISIBLE);
-//                break;
+
+            case R.id.quit:
+                intent = new Intent(MainActivity.this, PhoneActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.cre_db:
+//                mydbHelper.getWritableDatabase();
+                insert();
+                break;
         }
+    }
+
+    public void insert(){
+        String str = "{\"loginResult\":1,\"userInfo\":\"{\\\"Seq\\\":\\\"PA00000001\\\",\\\"Phone\\\":\\\"15277336128\\\",\\\"Password\\\":\\\"1234\\\",\\\"LoginWays\\\":1,\\\"RegisterDate\\\":\\\"Feb 10, 2020 12:00:00 AM\\\",\\\"Sex\\\":\\\"m\\\",\\\"Identity\\\":\\\"学生\\\",\\\"EcyPhone\\\":\\\"18877346041\\\",\\\"Name\\\":\\\"燕明\\\",\\\"IdentityId\\\":\\\"1600300333\\\"}\"}";
+        JSONObject jsonObject = JSONObject.parseObject(str);
+        JSONObject jsonUser = jsonObject.getJSONObject("userInfo");
+        Integer loginResult = jsonObject.getInteger("loginResult");
+//        System.out.println(loginResult);
+        Gson gson = new Gson();
+        User user = gson.fromJson(jsonUser.toString(), User.class);
+//        System.out.println(user.toString());
+
+        SQLiteDatabase db=null;
+        try{
+            db = mydbHelper.getWritableDatabase();
+            db.beginTransaction();
+
+            String sql = "insert into user_info(phone,sequence,password,login_ways,register_date,sex,identity,emergency_phone,name,identity_id)"
+                    +" values (?,?,?,?,?,?,?,?,?,?)";
+            db.execSQL(sql,
+                    new Object[]{user.getPhone(),user.getUserSeq(),user.getPassword(),user.getLoginWays(),user.getRegisterDate(),user.getSex(),user.getIdentity(),user.getEcyPhone(),user.getName(),user.getIdentityId()});
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            db.setTransactionSuccessful();
+            db.endTransaction();
+            db.close();
+        }
+
+
     }
 
     public void InitDrawer(){
         personalInfo = findViewById(R.id.PersonalInfo);
         personalInfo.setOnClickListener(this);
-//        drawer_fog = findViewById(R.id.drawer_fog);
         mDrawerLayout = findViewById(R.id.drawer_layout);
         mNavigation = findViewById(R.id.nav_view);
-
+        QuitTv = mNavigation.findViewById(R.id.quit);
+        QuitTv.setOnClickListener(this);
         mNavigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
                 mDrawerLayout.closeDrawers();
-//                drawer_fog.setVisibility(View.INVISIBLE);
                 return true;
             }
         });
