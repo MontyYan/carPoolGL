638dfb8c07b9daada269d3d549cd5cbf848b2bc5
MontyYan <839343988@qq.com>
2020-04-17 23:16:24 +0800

    main activity

--- app/src/main/java/com/example/carpoolgl/MainActivity.java
+++ app/src/main/java/com/example/carpoolgl/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.carpoolgl;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -14,26 +16,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.alibaba.fastjson.JSONObject;
-import com.amap.api.services.core.LatLonPoint;
+import com.example.carpoolgl.Static.STATIC_USERINFO;
 import com.example.carpoolgl.bean.User;
 import com.example.carpoolgl.dataBase.MydbHelper;
+import com.example.carpoolgl.main.mainPresenter;
+import com.example.carpoolgl.main.mainView;
 import com.example.carpoolgl.nowLoc.nowLocPresenter;
 import com.example.carpoolgl.nowLoc.nowLocView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.gson.Gson;
-
 import org.xutils.view.annotation.ViewInject;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, nowLocView {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, nowLocView, mainView {
+    private static final String TAG="MainActivity";
 
     private TextView getOnTv;
     private TextView getOffTv;
     private ImageButton personalInfo;
-//    private TextView drawer_fog;
     private nowLocPresenter nowLocP;
     private String nowLocation;
     //drawelayout不覆盖
@@ -41,31 +44,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private NavigationView mNavigation;
     private TextView QuitTv;
     private double mlatLong[]=new double[2];//记录当前位置的经纬度
-
     //创建数据库测试
     private Button cre_db_Test;
     private MydbHelper mydbHelper;
+
+    //个人用户数据控件
+    private TextView PhoneNum_Navheader_tv;
+    private TextView regDate_Navheader_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InitDrawer();
+        initController();
+        //创建数据库button test
+//        cre_db_Test = findViewById(R.id.cre_db);
+//        cre_db_Test.setOnClickListener(this);
+        findLocInfo();
+        mainPresenter mp = new mainPresenter(MainActivity.this);
+        mp.attachView(this);
+        mp.findLocUInfo();
+        mp.detachView();
+        nowLocP = new nowLocPresenter(getApplicationContext());
+        nowLocP.attachView(this);
+    }
+
+    public void initController(){
         getOnTv = findViewById(R.id.getOn_TV);
         getOnTv.setOnClickListener(this);
         getOffTv = findViewById(R.id.getOff_TV);
         getOffTv.setOnClickListener(this);
-
-        //创建数据库button test
-//        cre_db_Test = findViewById(R.id.cre_db);
-//        cre_db_Test.setOnClickListener(this);
-
-        mydbHelper = new MydbHelper(this,"user.db",null,1);
-//        QuitTv.findViewById(R.id.quit);
-//        QuitTv.setOnClickListener(this);
-        InitDrawer();
-        nowLocP = new nowLocPresenter(getApplicationContext());
-        nowLocP.attachView(this);
-
     }
 
     @Override
@@ -74,28 +83,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intent;
         switch (v.getId()){
             case R.id.getOn_TV:
-                flag = false;
-                intent = new Intent(MainActivity.this,SearchActivity.class);
-                intent.putExtra("Edit_select",flag);
-                intent.putExtra("nowLocation",nowLocation);
-                intent.putExtra("nowLatLong",mlatLong);
-                startActivity(intent);
+                if(!STATIC_USERINFO.getCon().equals(0)){
+                    flag = false;
+                    intent = new Intent(MainActivity.this,SearchActivity.class);
+                    intent.putExtra("Edit_select",flag);
+                    intent.putExtra("nowLocation",nowLocation);
+                    intent.putExtra("nowLatLong",mlatLong);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(MainActivity.this,"请先登录账号",Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.getOff_TV:
-                flag = true;
-                intent = new Intent(MainActivity.this,SearchActivity.class);
-                intent.putExtra("Edit_select",flag);
-                intent.putExtra("nowLocation",nowLocation);
-                intent.putExtra("nowLatLong",mlatLong);
-                startActivity(intent);
+                if(!STATIC_USERINFO.getCon().equals(0)){
+                    flag = true;
+                    intent = new Intent(MainActivity.this,SearchActivity.class);
+                    intent.putExtra("Edit_select",flag);
+                    intent.putExtra("nowLocation",nowLocation);
+                    intent.putExtra("nowLatLong",mlatLong);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(MainActivity.this,"请先登录账号",Toast.LENGTH_SHORT).show();
+                }
+
                 break;
             case R.id.PersonalInfo:
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 break;
 
             case R.id.quit:
-                intent = new Intent(MainActivity.this, PhoneActivity.class);
-                startActivity(intent);
+//                intent = new Intent(MainActivity.this, PhoneActivity.class);
+//                startActivity(intent);
+                quitDialog();
                 break;
 
 //            case R.id.cre_db:
@@ -105,21 +124,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    //主页面点击退出登录时的弹出窗口，用于再次确认
+    public void quitDialog(){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setTitle("确认退出");
+        dialog.setMessage("退出后，您的本地消息将被清空");
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(MainActivity.this, PhoneActivity.class);
+                startActivity(intent);
+            }
+        });
+        dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        dialog.show();
+    }
+
+    //测试用，后删
     public void insert(){
+        mydbHelper = new MydbHelper(this,"user.db",null,1);
         String str = "{\"loginResult\":1,\"userInfo\":\"{\\\"Seq\\\":\\\"PA00000001\\\",\\\"Phone\\\":\\\"15277336128\\\",\\\"Password\\\":\\\"1234\\\",\\\"LoginWays\\\":1,\\\"RegisterDate\\\":\\\"Feb 10, 2020 12:00:00 AM\\\",\\\"Sex\\\":\\\"m\\\",\\\"Identity\\\":\\\"学生\\\",\\\"EcyPhone\\\":\\\"18877346041\\\",\\\"Name\\\":\\\"燕明\\\",\\\"IdentityId\\\":\\\"1600300333\\\"}\"}";
         JSONObject jsonObject = JSONObject.parseObject(str);
         JSONObject jsonUser = jsonObject.getJSONObject("userInfo");
         Integer loginResult = jsonObject.getInteger("loginResult");
-//        System.out.println(loginResult);
         Gson gson = new Gson();
         User user = gson.fromJson(jsonUser.toString(), User.class);
-//        System.out.println(user.toString());
-
         SQLiteDatabase db=null;
         try{
             db = mydbHelper.getWritableDatabase();
             db.beginTransaction();
-
             String sql = "insert into user_info(phone,sequence,password,login_ways,register_date,sex,identity,emergency_phone,name,identity_id)"
                     +" values (?,?,?,?,?,?,?,?,?,?)";
             db.execSQL(sql,
@@ -132,10 +171,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             db.endTransaction();
             db.close();
         }
-
-
     }
 
+    public void findLocInfo(){
+//        PhoneNum_Navheader_tv.setText(STATIC_USERINFO.getPhone()+"");
+        PhoneNum_Navheader_tv.setText("aksdjhfadsh");
+//        regDate_Navheader_tv.setText("注册日期："+STATIC_USERINFO.getRegisterDate());
+        regDate_Navheader_tv.setText("ahdsufhiasufd");
+    }
+
+    //DrawerLayout滑动窗口
     public void InitDrawer(){
         personalInfo = findViewById(R.id.PersonalInfo);
         personalInfo.setOnClickListener(this);
@@ -146,20 +191,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mNavigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
                 mDrawerLayout.closeDrawers();
                 return true;
             }
         });
+
+        View nav_header = mNavigation.inflateHeaderView(R.layout.nav_header_main);
+//        View headLayout = mNavigation.getHeaderView(0);
+        Log.i(TAG,nav_header+"");
+        LinearLayout layout = nav_header.findViewById(R.id.header_layout);
+        PhoneNum_Navheader_tv = layout.findViewById(R.id.PhoneNum_Navheader_tv);
+        Log.i(TAG,PhoneNum_Navheader_tv+"");
+        regDate_Navheader_tv = layout.findViewById(R.id.regDate_Navheader_tv);
+        Log.i(TAG,regDate_Navheader_tv+"");
+        PhoneNum_Navheader_tv.setText("dsahfashdfo");
+        Log.i(TAG,PhoneNum_Navheader_tv.getText().toString());
+        regDate_Navheader_tv.setText("9283r98e79q");
+        Log.i(TAG,regDate_Navheader_tv.getText().toString());
     }
 
+    //设置全局静态
+    @Override
+    public void SetUInfo() {
+        PhoneNum_Navheader_tv.setText(STATIC_USERINFO.getPhone());
+        regDate_Navheader_tv.setText("注册日期："+STATIC_USERINFO.getRegisterDate());
+    }
+
+    //toast当前中文地址
     @Override
     public void setGetonText(String addr) {
         nowLocation = addr;
         getOnTv.setText(addr);
         Toast.makeText(this,addr,Toast.LENGTH_LONG).show();
     }
-
+    //读取当前经纬度
     @Override
     public void setLatLon(double lon, double lat) {
         mlatLong[0]=lon;
@@ -189,5 +254,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
     }
+
 
 }
