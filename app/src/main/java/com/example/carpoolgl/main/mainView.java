edc50281ccc5459c68a4fdce567c40b2dcbcce84
MontyYan <839343988@qq.com>
2020-04-18 11:52:40 +0800

    mainV

---
+++ app/src/main/java/com/example/carpoolgl/main/mainView.java
@@ -0,0 +1,8 @@
+package com.example.carpoolgl.main;
+
+import com.example.carpoolgl.base.baseView;
+
+public interface mainView extends baseView {
+    //用于M层回调activityV层，设置用户全局静态信息
+    void SetUInfo();
+}
