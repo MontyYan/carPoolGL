c575a155129c28aa943acb3083cc9d0117fdcda1
MontyYan <839343988@qq.com>
2020-05-02 10:34:42 +0800

    nowlocMvp

--- app/src/main/java/com/example/carpoolgl/nowLoc/nowLocPresenter.java
+++ app/src/main/java/com/example/carpoolgl/nowLoc/nowLocPresenter.java
@@ -8,7 +8,6 @@ import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
-import com.amap.api.services.core.LatLonPoint;
 
 public class nowLocPresenter {
     private AMapLocationClient mClient;
--- app/src/main/java/com/example/carpoolgl/nowLoc/nowLocView.java
+++ app/src/main/java/com/example/carpoolgl/nowLoc/nowLocView.java
@@ -1,7 +1,5 @@
 package com.example.carpoolgl.nowLoc;
 
-import com.amap.api.services.core.LatLonPoint;
-
 public interface nowLocView {
     void setGetonText(String addr);
     void setLatLon(double lat, double lon);
